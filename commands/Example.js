f1f3eeae616839d2bb6a093e46176764a5d11651
cart1305 <boristheotherslave@gmail.com>
2022-02-05 15:49:34 +0700

    idk

--- commands/Example.js
+++ commands/Example.js
@@ -1,12 +1,13 @@
 const Command = require("./Command");
+const Vec3 = require("vec3");
 
 class ExampleCommand extends Command {
     constructor() {
-        super("ExampleCommand", "An example command");
+        super("clip", "An example command");
     }
 
     onExecute([...args]) {
-        console.log(args);
+        this.bot.entity.position.add(new Vec3(args[0], args[1], args[2]))
     }
 }
 
